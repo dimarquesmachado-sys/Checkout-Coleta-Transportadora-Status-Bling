52dab04d82c779540f9305c5f096f12805866b43
dimarquesmachado-sys <dimarquesmachado@gmail.com>
2026-04-02 19:52:40 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/callback', async (req, res) => {
